bfa1bf8fcb858a4ffab475ea0511dc2286a8a8ea
gokul <nitha.nair@ismoman.com>
2025-01-12 17:10:53 +0400

    commiut

--- lesson 6/indntifier.py
+++ lesson 6/indntifier.py
@@ -1,1 +1,11 @@
-
+x=5
+if type (x)is int:print("True")
+else:print("False")
+x=5.0
+if type (x) is not float:print("True")
+else print("False")
+x=20
+y=20
+if x is y:print("x and y same identity")
+y=30
+if x is not y print("x and y are diferent identity")
