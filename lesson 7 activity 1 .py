bc0c10d481a80c31b4783d56ebfab428743d5c25
gokul <nitha.nair@ismoman.com>
2025-01-19 17:06:29 +0400

    first publish

---
+++ lesson 7 activity 1 .py
@@ -0,0 +1,16 @@
+v=4
+w=5
+x=8
+y=2
+z=0
+z=(v+w)*x/y
+print("value of (v+w)*x/y is ",z)
+
+
+
+Name = "alex"
+age = 0
+if Name == "alex" or Name =="John" and age >=2:
+    print("Hello welcome")
+else:
+    print("Good bye")
