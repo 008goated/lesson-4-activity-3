91d636a9e330f0c638009ec23ca040fc2fcc97d3
gokul <nitha.nair@ismoman.com>
2025-01-26 13:37:47 +0400

    first commit

---
+++ project 2.py
@@ -0,0 +1,10 @@
+shayaan="23 may"
+dhaiwik="28 august"
+uttkarsha="19 december"
+vidhura="7 january"
+noble="19 may"
+print(shayaan,"best of luck")
+print(dhaiwik,"best of luck")
+print(uttkarsha,"best of luck")
+print(vidhura,"best of luck")
+print(noble,"best of luck")
